9ed63a0bddba350cb263a5dd7792040e77b9d174
José Ramos <02.jrsramos@gmail.com>
2023-02-17 11:26:50 -0300

    v1.1

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -3,12 +3,12 @@ import ResumeItem from '../ResumeItem';
 import * as C from "./styles";
 import { FaRegArrowAltCircleDown, FaRegArrowAltCircleUp, FaDollarSign } from 'react-icons/fa';
 
-const Resume = () => {
+const Resume = ({income, expense, total}) => {
     return (
         <C.Container>
-            <ResumeItem value="1000" title="Entradas" Icon={FaRegArrowAltCircleUp}/>
-            <ResumeItem value="1000" title="Saidas" Icon={FaRegArrowAltCircleDown}/>
-            <ResumeItem value="1000" title="Total" Icon={FaDollarSign}/>
+            <ResumeItem value={income} title="Entradas" Icon={FaRegArrowAltCircleUp}/>
+            <ResumeItem value={expense} title="Saidas" Icon={FaRegArrowAltCircleDown}/>
+            <ResumeItem value={total} title="Total" Icon={FaDollarSign}/>
         </C.Container>
     );
 }
